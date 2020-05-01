195695cf4cb9d97d7c4f32fae250419602a80c2a
Juliette Marrie <jmarrie@pe2cc3-043.c.nygenome.org>
2020-05-01 08:33:56 -0400

    Separated util in two.

--- util.py
+++ util.py
@@ -1,4 +1,11 @@
 import numpy as np
+from read_util import (
+    covers_str,
+    expanded_seq,
+    parse_changes,
+    parse_insertions,
+    fetch_indels
+)
 
 
 def error_correction(vcf, bam, log_every=1000):
@@ -126,29 +133,6 @@ def fetch_reads(bam, site, str_info, filter_dups=False, match_alt=False):
             yield read
 
 
-def get_real_start(read):
-    start = read.reference_start
-    cigar = read.cigartuples
-    if cigar[0][0] == 4:
-        start -= cigar[0][1]
-    return start
-
-
-def get_real_end(read):
-    end = read.reference_end
-    cigar = read.cigartuples
-    if cigar[-1][0] == 4:
-        end += cigar[-1][1]
-    return end
-
-
-def covers_str(read, str_info):
-    return (
-        read.reference_end > str_info["stop"]
-        and read.reference_start < str_info["start"]
-    )
-
-
 def del_length_one(dict_):
     for key in [k for k in dict_.keys()]:
         if len(dict_[key]) <= 1:
@@ -165,86 +149,6 @@ def collect_reference_STRs(bed):
     ):
         reference_STRs[region["STR_id"]] = region
     return reference_STRs
-
-
-def expanded_seq(read, region):
-    cigar_tup_dict = {i: s for i, s in enumerate(["M", "I", "D", "N", "S"])}
-    start, end = read.reference_start, read.reference_end
-
-    sequence = read.query_alignment_sequence
-    cigar = read.cigartuples
-
-    if cigar[0][0] == 4:
-        cigar = cigar[1:]
-    if cigar[-1][0] == 4:
-        cigar = cigar[:-1]
-
-    j = 0
-    i = start
-    seq_str = ""
-    cig_str = ""
-
-    k = 0
-
-    for operator, length in cigar:
-        operator = cigar_tup_dict[operator]
-        k += length
-        if operator == "D":
-            seq_str += "X" * length
-            i += length
-
-        elif operator == "I":
-            seq_str += sequence[j : j + length].lower()
-            j += length
-        else:
-            seq_str += sequence[j : j + length]
-            j += length
-            i += length
-        cig_str += operator * length
-
-    front_clip, back_clip = (region[0] - start), (region[1] - end)
-    if front_clip > 0:
-        seq_str = seq_str[front_clip:]
-        cig_str = cig_str[front_clip:]
-    elif front_clip < 0:
-        seq_str = abs(front_clip) * "_" + seq_str
-        cig_str = abs(front_clip) * "_" + cig_str
-    if back_clip < 0:
-        seq_str = seq_str[:back_clip]
-        cig_str = cig_str[:back_clip]
-    elif back_clip > 0:
-        seq_str += back_clip * "_"
-        cig_str += back_clip * "_"
-
-    return seq_str, cig_str
-
-
-def parse_changes(cigar, C):
-    changes = 0
-    for c in cigar:
-        if c == C:
-            changes += 1
-    return changes
-
-
-def parse_insertions(f_cig, m_cig, e_cig):
-    for c in f_cig[::-1]:
-        if c != "I":
-            break
-        else:
-            m_cig = c + m_cig
-    for c in e_cig:
-        if c != "I":
-            break
-        m_cig += c
-    return parse_changes(m_cig, "I")
-
-
-def fetch_indels(read, region):
-    ref, cig = expanded_seq(read, region)
-    deletions = parse_changes(cig[9:-11], "D")
-    insertions = parse_insertions(cig[:9], cig[9:-11], cig[-11:])
-    return deletions, insertions
 
 
 def overlap_noise(family, site, unique=False):
